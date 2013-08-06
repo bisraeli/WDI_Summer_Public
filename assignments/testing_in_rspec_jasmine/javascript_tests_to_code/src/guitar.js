e3d02c3149375d8f8821e9fc49d5c20bde2a87f2
Ben Israeli <jisraeli@gmail.com>
2013-08-06 11:33:00 -0400

    only have 2 specs need to repull?

--- assignments/testing_in_rspec_jasmine/javascript_tests_to_code/src/guitar.js
+++ assignments/testing_in_rspec_jasmine/javascript_tests_to_code/src/guitar.js
@@ -7,4 +7,17 @@ if (typeof Object.create !== 'function') {
 }
 
 var guitar = {
+  test_guitar = Object.create(guitar);
+  numberOfStrings = 6
+  var breakString = function(numberOfStrings){
+    numberOfStrings - 1
+  }
+  var playbasicRhythm = function(){
+    console.log("jug jigga jug jigga jug")
+  }
+  var playhighstrings = function(){
+    console.log("meedley meedley meedley meedley meedley meedley meedley meedley meedley meedley meedley meedley MEEEEEEEEEEEEEEEE. \'And the dragon comes in the NIIIiiiiIIIiiiiIIIIIIIIiiiiiiiiiiiIIIIIIIIiiiIIGGHH\')
+  }
+
+
 };
